248683592fb25ff91f7e0f97a25dc7f89d904928
Demonslyr <Demonslyr@users.noreply.github.com>
2021-07-24 23:44:15 -0500

    Feature/initial api call (#8)
    * incremental commit
    
    Co-authored-by: Demonslyr <Demonslyr@users.noreply.github.com>
    
    * fixed example api response handling
    
    * added toast example
    
    Co-authored-by: aruss314 <32945295+aruss314@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import MainContainer from './containers/MainContainer';
+import { ToastContainer } from 'react-toastify';
+import Container  from 'react-bootstrap/Container';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className="App-header">
+      <ToastContainer />
+      <MainContainer />
+    </Container>
   );
 }
 
