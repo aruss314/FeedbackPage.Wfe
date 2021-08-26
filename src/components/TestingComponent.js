6d0f7bd9fff7205f8dbbc984cea7c2ea51907176
aruss314 <32945295+aruss314@users.noreply.github.com>
2021-08-25 19:32:43 -0500

    implemented the Reset Board Button

--- src/components/TestingComponent.js
+++ src/components/TestingComponent.js
@@ -2,110 +2,119 @@
 import { Button } from 'bootstrap';
 import React from 'react'
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square" onClick={() => this.props.onClick()} >
-          {this.props.value}
-        </button>
-      );
-    }
+function Square({ onClick: onSquareClick, value }) {
+  return (
+    <button className="square" onClick={onSquareClick} >
+      {value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+
+  constructor(props) {
+    super(props);
   }
-  
-  class Board extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          squares: ['r', 'q', 'k', 'r', 
-                    'v', 'v', 'v', 'v',
-                    '^', '^', '^', '^',
-                    'R','Q','K','R'       ]
-        };
-      }
+  renderSquare(i) {
+    return <Square value={this.props.squares[i]} onClick={() => this.props.handleSquareClick(i)} />;
+  }
 
-      handleClick(i){
-          const squares = this.state.squares.slice();
-          squares[i] = 'x';
-          this.setState({ squares: squares});
-      }
+  render() {
+    const status = 'Next player: X';
 
-    renderSquare(i) {
-      return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-            {this.renderSquare(3)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(8)}
-            {this.renderSquare(9)}
-            {this.renderSquare(10)}
-            {this.renderSquare(11)}
-            </div>
-            <div className="board-row">
-            {this.renderSquare(12)}
-            {this.renderSquare(13)}
-            {this.renderSquare(14)}
-            {this.renderSquare(15)}
-          </div>
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
         </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
+        <div className="board-row">
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
         </div>
-      );
-    }
+        <div className="board-row">
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+          {this.renderSquare(10)}
+          {this.renderSquare(11)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(12)}
+          {this.renderSquare(13)}
+          {this.renderSquare(14)}
+          {this.renderSquare(15)}
+        </div>
+      </div>
+    );
   }
-  
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
 
 class TestingComponent extends React.Component {
 
-  resetTheBoard() { 
-        alert( 'board reset') }
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: this.defaultBoard
+    }
+  }
 
-    render() {
-      return (
-          <>
-        <Board />
+  defaultBoard = ['r', 'q', 'k', 'r',
+    'v', 'v', 'v', 'v',
+    '^', '^', '^', '^',
+    'R', 'Q', 'K', 'R']
 
-        <button 
-        onClick=  /* {() => alert('Resetting the board.')} */
-                 { this.resetTheBoard() }
+  resetTheBoard = () => {
+    alert('board reset');
+    this.setState({
+      squares: this.defaultBoard
+    });
+  }
+
+  handleClick=(i) => {
+    const squares = this.state.squares.slice();
+    squares[i] = 'x';
+    this.setState({ squares: squares });
+  }
+
+  render() {
+    const { squares } = this.state;
+    return (
+      <>
+        <Board squares={squares} handleSquareClick={this.handleClick}  />
+
+        <button
+          onClick=  /* {() => alert('Resetting the board.')} */
+          {this.resetTheBoard}
         >
           Click here to reset the board.
         </button>
 
-        </>
-      );
-    }
+      </>
+    );
   }
-  
-  export default TestingComponent;
+}
+
+export default TestingComponent;
